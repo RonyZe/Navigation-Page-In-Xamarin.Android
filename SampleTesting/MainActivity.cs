a254ee4652208d35c8cc73f39b3dbc08b78099c6
RonyZe <zrony58@gmail.com>
2019-01-09 19:20:30 +0500

    Add project files.

---
+++ SampleTesting/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Telecom;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace SampleTesting
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+
+//            EditText email = FindViewById<EditText>(Resource.Id.userName);
+//            EditText pass = FindViewById<EditText>(Resource.Id.password);           
+            Button loginButton = FindViewById<Button>(Resource.Id.login);
+
+            loginButton.Click += (sender, e) =>
+            {
+                Intent nextActivity = new Intent(this, typeof(other));
+                //                nextActivity.PutExtra("email", email.Text);
+                //                nextActivity.PutExtra("password", pass.Text);   
+
+                StartActivity(nextActivity);
+            };
+
+
+        }
+    }
+}
---
+++ SampleTesting/other.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SampleTesting
+{
+    [Activity(Label = "other")]
+    public class other : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.OtherPage);
+//            string email = Intent.GetStringExtra("email" ?? "Not Rec");
+//            string pass = Intent.GetStringExtra("password" ?? "Not Rec");
+//
+//            TextView texte = FindViewById<TextView>(Resource.Id.textEmail);
+//            TextView textp = FindViewById<TextView>(Resource.Id.textPassword);
+            Button goback = FindViewById<Button>(Resource.Id.btnGoBack);
+
+
+//            texte.Text = email;
+//            textp.Text = pass;
+
+            goback.Click += delegate { this.Finish(); };
+
+
+
+        }
+    }
+}
